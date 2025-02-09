973839bd80aa306c6e4783aac7a5157b425eb5cf
Felipe Sanz <124616676+Multydelivery@users.noreply.github.com>
2025-02-09 16:55:45 -0500

    removed db.js from .gitignore

---
+++ food-del/backend/config/db.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+
+export const  connectDB = async () =>{
+
+    await mongoose.connect(`${process.env.MONGODB_URI}/food-del`).then(()=>console.log("DB Connected"));
+   
+}
