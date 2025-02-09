bfd6fcf592268a8b56a6d351fa642fc1c6d66820
Felipe Sanz <124616676+Multydelivery@users.noreply.github.com>
2025-02-09 18:04:16 -0500

    added cloudinary to storage files

--- food-del/backend/routes/foodRoute.js
+++ food-del/backend/routes/foodRoute.js
@@ -1,21 +1,12 @@
 import express from 'express';
 import { addFood, listFood, removeFood } from '../controllers/foodController.js';
-import multer from 'multer';
+import upload from '../config/cloudinary.js'; // Import the Cloudinary configuration
+
 const foodRouter = express.Router();
 
-//Image Storage Engine (Saving Image to uploads folder & rename it)
-
-const storage = multer.diskStorage({
-    destination: 'uploads',
-    filename: (req, file, cb) => {
-        return cb(null,`${Date.now()}${file.originalname}`);
-    }
-})
-
-const upload = multer({ storage: storage})
-
-foodRouter.get("/list",listFood);
-foodRouter.post("/add",upload.single('image'),addFood);
-foodRouter.post("/remove",removeFood);
+// Use Cloudinary for image uploads
+foodRouter.get("/list", listFood);
+foodRouter.post("/add", upload.single('image'), addFood);
+foodRouter.post("/remove", removeFood);
 
 export default foodRouter;
