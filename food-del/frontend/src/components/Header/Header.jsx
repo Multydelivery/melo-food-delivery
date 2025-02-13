39eb5e351e948fa998a8ce1fafd223be6dcf9233
Felipe Sanz <124616676+Multydelivery@users.noreply.github.com>
2025-02-13 12:39:59 -0500

    added more food items

--- food-del/frontend/src/components/Header/Header.jsx
+++ food-del/frontend/src/components/Header/Header.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import './Header.css'
 
 const Header = () => {
+
+    const viewMenu = () => {
+        window.scrollTo(0,document.getElementById('explore-menu').offsetTop)
+    }
     return (
         <div className='header'>
             <div className='header-contents'>
                 <h2>Order your favourite food here</h2>
-                <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
-                <button>View Menu</button>
+                {/* <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p> */}
+                 <button onClick={viewMenu}>View Menu</button>
             </div>
         </div>
     )
