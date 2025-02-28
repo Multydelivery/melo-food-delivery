7e277ea84e06f9f0d0e88be115fcfbbec597a46b
Felipe Sanz <124616676+Multydelivery@users.noreply.github.com>
2025-02-27 21:34:24 -0500

    remove img from email template

--- food-del/backend/config/nodemailer.js
+++ food-del/backend/config/nodemailer.js
@@ -36,7 +36,7 @@ export const sendOrderNotification = async (orderDetails, userEmail) => {
       subject: 'Order Confirmation',
       html: `
         <div style="text-align: center;">
-          <img src="${process.env.API_URL}/header_img.png" alt="Logo" style="width: 150px; height: auto;"/>
+         <p></p>
         </div>
         <h1>Order Confirmation</h1>
         <p>Thank you for your order, <strong>${orderDetails.name}</strong>!</p> <!-- Add name here -->
