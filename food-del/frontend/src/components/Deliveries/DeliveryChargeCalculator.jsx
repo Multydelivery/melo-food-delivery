4bb2d48552b23d81cc38248e7c6dbac84c76c6bd
Felipe Sanz <124616676+Multydelivery@users.noreply.github.com>
2025-03-02 17:48:31 -0500

    added new zipcode availability

--- food-del/frontend/src/components/Deliveries/DeliveryChargeCalculator.jsx
+++ food-del/frontend/src/components/Deliveries/DeliveryChargeCalculator.jsx
@@ -4,14 +4,16 @@ const DeliveryChargeCalculator = ({ zipCode }) => {
   // Delivery charges based on zip codes
   const deliveryCharges = {
     "07631": 5.00, // Englewood, NJ
-    "07666": 7.00, // Englewood, NJ (shared with Teaneck)
-    "07608": 7.00, // Teaneck, NJ
-    "07621": 7.00, // Bergenfield, NJ
-    "07670": 7.00, // Tenafly, NJ
-    "07601": 15.00, // Hackensack, NJ
-    "07652": 7.00, // Paramus, NJ
-    "07450": 7.00, // Ridgewood, NJ
-    "07650": 7.00, // Palisades Park, NJ
+    "07666": 8.00, // Englewood, NJ (shared with Teaneck)
+    "07608": 8.00, // Teaneck, NJ
+    "07621": 8.00, // Bergenfield, NJ
+    "07670": 8.00, // Tenafly, NJ
+    "07601": 6.00, // Hackensack, NJ
+    "07632": 8.00, // Englewood Cliffs, NJ
+    "07605": 12.00, // Leonia, NJ
+    "07628": 15.00, // Dumont, NJ
+    "07646": 15.00, // New Milford, NJ
+
   };
 
   // Calculate delivery charge based on zip code
@@ -36,15 +38,17 @@ const DeliveryChargeCalculator = ({ zipCode }) => {
 DeliveryChargeCalculator.calculateDeliveryCharge = (zip) => {
   const deliveryCharges = {
     "07631": 5.00, // Englewood, NJ
-    "07666": 7.00, // Englewood, NJ (shared with Teaneck)
-    "07608": 7.00, // Teaneck, NJ
-    "07621": 7.00, // Bergenfield, NJ
-    "07670": 7.00, // Tenafly, NJ
-    "07601": 15.00, // Hackensack, NJ
-    "07652": 7.00, // Paramus, NJ
-    "07450": 7.00, // Ridgewood, NJ
-    "07650": 7.00, // Palisades Park, NJ
+    "07666": 8.00, // Englewood, NJ (shared with Teaneck)
+    "07608": 8.00, // Teaneck, NJ
+    "07621": 8.00, // Bergenfield, NJ
+    "07670": 8.00, // Tenafly, NJ
+    "07601": 6.00, // Hackensack, NJ
+    "07632": 8.00, // Englewood Cliffs, NJ
+    "07605": 12.00, // Leonia, NJ
+    "07628": 15.00, // Dumont, NJ
+    "07646": 15.00, // New Milford, NJ
   };
+    
   return deliveryCharges[zip] || null; // Return null if the zip code is not found
 };
 
